9eb56176d9db4eb57471264d0e6a91206a6bb7d8
sahil-patel620 <Sp453771@gmail.com>
2025-08-19 09:20:58 +0530

    dice game almost completed

--- dice_game/src/App.jsx
+++ dice_game/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from "styled-components";
+import StartGame from "./components/StartGame";
+import GamePlay from "./components/GamePlay";
+import { useState } from "react";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[isGameStarted, setIsGameStarted] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const toggeleGamePlay = ()=>{
+    setIsGameStarted(prev => !prev);
+  }
+
+  return <>
+  {isGameStarted ? <GamePlay/> : <StartGame toggle= {toggeleGamePlay}/>}
+  </>
 }
 
-export default App
+export default App;
+
+const Button = styled.button`
+background-color: black;
+color: white;
+padding: 10px;
+`;
---
+++ dice_game/src/components/GamePlay.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import TotalScore from "./TotalScore";
+import NumberSelector from "./NumberSelector";
+import styled from "styled-components";
+import RoleDice from "./RoleDice";
+import {Button, OutlineButton} from "../styled/Button";
+import Rules from "./Rules";
+
+const GamePlay = () => {
+  const [selectedNumber, setSelectedNumber] = useState();
+  const [currentDice, setCurrentDice] = useState(1);
+  const [score, setScore] = useState(0);
+  const [error, setError] = useState("");
+  const [showRules, setShowRules] = useState(false);
+
+  const generateRandomNumber = () => {
+    return Math.floor(Math.random() * 6) + 1;
+  };
+
+  const roleDice = () => {
+    if (!selectedNumber) {
+      setError("You have not selected any number");
+      return;
+    }
+    const randomNumber = generateRandomNumber();
+    setCurrentDice((prev) => randomNumber);
+
+    if (selectedNumber == randomNumber) {
+      setScore((prev) => prev + randomNumber);
+    } else {
+      setScore((prev) => prev - 2);
+    }
+
+    setSelectedNumber(undefined);
+  };
+
+  const resetScore = () => {
+    setScore(0);
+  }
+
+  return (
+    <MainContainer>
+      <div className="top_section">
+        <TotalScore score={score} />
+        <NumberSelector
+          error = {error}
+          setError = {setError}
+          selectedNumber={selectedNumber}
+          setSelectedNumber={setSelectedNumber}
+        />
+      </div>
+      <RoleDice currentDice={currentDice} roleDice={roleDice} />
+      <div className="btns">
+        <OutlineButton onClick={resetScore}>Reset Score</OutlineButton>
+        <Button onClick={()=>  setShowRules((prev)=> !prev)}>Show Rules</Button>
+      </div>
+     {showRules && <Rules/>}
+    </MainContainer>
+  );
+};
+
+export default GamePlay;
+
+const MainContainer = styled.main`
+  padding-top: 70px;
+  .top_section {
+    display: flex;
+    justify-content: space-around;
+    align-items: end;
+  }
+
+  .btns{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    margin-top: 40px;
+  }
+`;
