0734a9ed3fc201d418ac3bd29dee10e349c3c0fa
sahil-patel620 <Sp453771@gmail.com>
2025-08-20 10:33:52 +0530

    ev_landing_page

---
+++ ev_web_landing_page/src/App.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Background from "./Components/Background/Background";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+
+function App() {
+  let heroData = [
+    { text1: "Dive into", text2: "what you love" },
+    { text1: "Indulge", text2: "your passions" },
+    { text1: "Give in to", text2: "your passions" },
+  ];
+
+  const [heroCount, setHeroCount] = useState(0);
+  const [playStatus, setPlayStatus] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setHeroCount((count) => {
+        return count === 2 ? 0 : count + 1;
+      });
+      
+  return () => clearInterval(interval);
+    }, 3000);
+  }, []);
+
+  return (
+    <div>
+      <Background heroCount={heroCount} playStatus={playStatus} />
+      <Navbar />
+      <Hero
+        heroData={heroData[heroCount]}
+        heroCount={heroCount}
+        setHeroCount={setHeroCount}
+        playStatus={playStatus}
+        setPlayStatus={setPlayStatus}
+      />
+    </div>
+  );
+}
+
+export default App;
